2c6cf0dde66a950c3fa12e1f858f223dc4388edd
Ethan Shin <shinhc5@gmail.com>
2020-11-18 00:45:42 +0900

    :lipstick: styling Login, Button components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-function Button({ onClick, text }) {
-  return (<button onClick={onClick}>{text}</button>);
+function Button({ onClick, text, color }) {
+  return (<StyledButton color={color} onClick={onClick}>{text}</StyledButton>);
 }
+
+const StyledButton = styled.button`
+  all: unset;
+  cursor: pointer;
+  max-width: 100px;
+  padding: 14px 16px;
+  border-radius: 32px;
+  font-size: 14px;
+  background-color: ${props => props.color || '#ffd32a'};
+  transition-property: scale, translateY;
+  transition: filter 0.3s;
+
+  &:hover {
+    filter: brightness(0.7);
+  }
+`;
 
 export default Button;
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
+  color: PropTypes.string,
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import Button from './Button';
 
 function Login({ onLogin }) {
   return (
-    <div>
-      <h1>술톡</h1>
-      <p>친구와 함께하는 술술TALK🍺</p>
-      <Button onClick={onLogin} text='구글 로그인' />
-    </div>
+    <Container>
+      <div>
+        <h1>술톡</h1>
+        <p>친구와 함께하는 술술TALK🍺</p>
+        <Button onClick={onLogin} text='구글 로그인' />
+      </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #330057;
+
+  div {
+    width: 380px;
+    height: 480px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 42px;
+    background-color: #49007d;
+  }
+
+  h1 {
+    font-size: 48px;
+    margin-bottom: 30px;
+    color: #f7d794;
+    text-align: center;
+  }
+
+  p {
+    font-size: 24px;
+    font-weight: 400;
+    margin-bottom: 50px;
+    color: #f7d794;
+  }
+`;
 
 export default Login;
 
