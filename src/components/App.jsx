197ecff70ed1ebd6c9cd8fef13d429e6ebd3d125
Ethan Shin <shinhc5@gmail.com>
2020-11-17 20:10:27 +0900

    :wrench: set initial configuration for styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
 
 import MyPageContainer from '../containers/MyPageContainer';
 import LobbyContainer from '../containers/LobbyContainer';
 import RoomContainer from '../containers/RoomContainer';
 import FloatingButton from './FloatingButton';
 import Login from './Login';
+
+import theme from './styles/theme';
 
 function App({ socket, user, onLogin, onLoad }) {
   const [isMyPageOpen, setMyPageOpen] = useState(false);
@@ -20,7 +24,8 @@ function App({ socket, user, onLogin, onLoad }) {
   }, [user]);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <Reset />
       {user && isMyPageOpen && <MyPageContainer />}
       {user && <FloatingButton onClick={() => setMyPageOpen(!isMyPageOpen)} text='나' />}
       <Switch>
@@ -32,7 +37,7 @@ function App({ socket, user, onLogin, onLoad }) {
         </Route>
         <Redirect to='/' />
       </Switch>
-    </>
+    </ThemeProvider>
   );
 }
 
