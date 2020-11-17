9f2b47c685368ae366579620059e981b6b380844
Ethan Shin <shinhc5@gmail.com>
2020-11-18 00:44:37 +0900

    :wrench: set globalStyle.js

---
+++ src/components/styles/globalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Nanum Gothic Coding', monospace;
+    font-weight: 700;
+  }
+`;
+
+export default GlobalStyle;
