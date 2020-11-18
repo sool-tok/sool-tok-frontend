6dd259800d75ff0ce3912ce1ab0ca0ee1920739f
Ethan Shin <shinhc5@gmail.com>
2020-11-18 12:12:55 +0900

    :heavy_plus_sign: add react-icons

--- src/components/styles/globalStyle.js
+++ src/components/styles/globalStyle.js
@@ -2,7 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    font-family: 'Nanum Gothic Coding', monospace;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
+    Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     font-weight: 700;
   }
 `;
